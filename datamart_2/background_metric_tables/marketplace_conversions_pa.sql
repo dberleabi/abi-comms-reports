1160a8f9cdab445ddfabe4f64b77c411b471f664
dberleabi <72942563+dberleabi@users.noreply.github.com>
2022-02-03 18:20:40 -0500

    Update marketplace_conversions_pa.sql

--- datamart_2/background_metric_tables/marketplace_conversions_pa.sql
+++ datamart_2/background_metric_tables/marketplace_conversions_pa.sql
@@ -1,1 +1,2 @@
-
+--This table contains marketplace conversions using product added as the event where the conversion event is within 1 days of the impression
+-- A marketplace conversion is defined as an product added event where the SKU is a marketplace SKU
