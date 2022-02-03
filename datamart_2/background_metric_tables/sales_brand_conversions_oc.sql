2b688f1fa5097b6c43ed4af13ec433625bdad932
dberleabi <72942563+dberleabi@users.noreply.github.com>
2022-02-03 18:23:10 -0500

    Create sales_brand_conversions_oc.sql

---
+++ datamart_2/background_metric_tables/sales_brand_conversions_oc.sql
@@ -0,0 +1,2 @@
+--This table contains sales brand conversions using order completed as the event where the conversion event is within 1 days of the impression
+-- A sales brand conversion is defined as an order completed event where the Brand matches the Brand tagged in the campaign
