deaa6b958d0f243459b2d6ec106ebeed913bf780
dberleabi <72942563+dberleabi@users.noreply.github.com>
2022-02-03 18:22:30 -0500

    Create sales_sku_conversions_oc.sql

---
+++ datamart_2/background_metric_tables/sales_sku_conversions_oc.sql
@@ -0,0 +1,2 @@
+--This table contains sales sku conversions using order completed as the event where the conversion event is within 7 days of the impression
+-- A sales sku conversion is defined as an order completed event where the SKU matches the SKU tagged in the campaign
