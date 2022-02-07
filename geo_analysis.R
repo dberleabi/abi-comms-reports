b7f5e11e72d0828493b6b2e3ab6f61965a33b034
dberleabi <72942563+dberleabi@users.noreply.github.com>
2022-02-07 14:07:48 -0500

    Add files via upload

---
+++ geo_analysis.R
@@ -0,0 +1,56 @@
+#set wd to abi
+setwd("/Users/danberle/abi")
+
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+library(readr)
+library(lubridate)
+library(sf)
+
+#Read in sample geo data
+sample_geo <- read_csv("br_sample_w_orders.csv")
+
+#Create an sf dataframe 
+sf_geo <- st_as_sf(sample_geo, coords = c("DELIVERY_LONGITUDE", "DELIVERY_LATITUDE"),
+                   crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+#Create a distance matrix
+dist.mat <- st_distance(sf_geo)
+
+#Number of points within 100 meters
+num.100 <- apply(dist.mat, 1, function(x) {
+  sum(x < 100) - 1
+})
+
+#Number of points within 10 meters
+num.10 <- apply(dist.mat, 1, function(x) {
+  sum(x < 10) - 1
+})
+
+#Calculate the nearest difference
+nn.dist <- apply(dist.mat, 1, function(x) {
+  return(sort(x, partial = 2)[2])
+})
+
+# Get index for nearest distance
+nn.index <- apply(dist.mat, 1, function(x) { order(x, decreasing=F)[2] })
+
+#Put the data together
+n.data <- sample_geo
+colnames(n.data)[1:ncol(n.data)] <- 
+  paste0("n.", colnames(n.data)[1:ncol(n.data)])
+
+sample_geo2 <- data.frame(sample_geo,
+                          n.data[nn.index, ],
+                          n.distance = nn.dist,
+                          radius100 = num.100,
+                          radius10 = num.10)
+
+#Create a table with POCs and their closest neighbor where that neighbor is within 10 meters
+sample_geo3 <- sample_geo2 %>%
+  select(COUNTRY:DELIVERY_LONGITUDE, n.ACCOUNT_ID, n.DELIVERY_LATITUDE, 
+         n.DELIVERY_LONGITUDE, n.distance, radius100, radius10
+         ) %>%
+  filter(DELIVERY_LATITUDE != 0) %>%
+  filter(n.distance <= 10)
